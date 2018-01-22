241c2d1f5d05a8224324b1ed465b8d9d55c2ca76
Ryan <ryanlwrnc@hotmail.com>
2018-01-21 17:16:49 -0800

    Fixed a missing NBA team.

--- getData.py
+++ getData.py
@@ -18,7 +18,7 @@ def main():
 
    teams = ["ATL", "BKN", "BOS", "CHA", "CHI", "CLE", "DAL", "DEN", "DET" , "GSW" ,
       "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOP", "NYK", "OKC", 
-      "ORL", "PHI", "PHX", "POR", "SAC", "SAS", "TOR", "UTA"]
+      "ORL", "PHI", "PHX", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
 
    # Get team gamelogs for all NBA teams
    all_team_gamelogs = {'allteamgamelogs':[]}
